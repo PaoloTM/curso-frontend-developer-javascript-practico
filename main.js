8d510932dfd33fcb3fbb8394260ec40039aacbdb
Paolo TM <paolotm56@gmail.com>
2023-06-03 22:48:26 -0300

    agregamos carrito de compras, y menu mobile + control de errores

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const cartMenuIcon = document.querySelector('.navbar-shopping-cart');
+const cartAsideMenu = document.querySelector('.product-detail');
 
-navEmail.addEventListener('click', toggleDesktopMenu);
+navEmail.addEventListener('click', () => toggleElement(desktopMenu, cartAsideMenu));
+mobileMenuIcon.addEventListener('click', () => toggleElement(mobileMenu, cartAsideMenu));
+cartMenuIcon.addEventListener('click', () => toggleElement(cartAsideMenu, mobileMenu));
 
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+function toggleElement(element, isOpen) {
+    element.classList.toggle('inactive');
+    isOpen.classList.add('inactive');
 }
